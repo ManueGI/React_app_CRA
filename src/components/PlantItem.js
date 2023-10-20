649a3941fdabf7ed1318cfdae4794db295613bb6
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2023-10-21 00:39:45 +0200

    style cart an shopping-list finished

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -4,7 +4,7 @@ import '../styles/PlantItem.css'
 
 function PlantItem({ id, cover, name, water, light }) {
 	return (
-		<li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
+		<li key={id} className='lmj-plant-card' onClick={() => handleClick(name)}>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
